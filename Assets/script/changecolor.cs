380142d36e24ac4c21fa1985e447cb7a1ff9b686
Gustavobtfelix <90421959+Gustavobtfelix@users.noreply.github.com>
2022-08-20 12:50:21 -0300

    Update changecolor.cs

--- Assets/script/changecolor.cs
+++ Assets/script/changecolor.cs
@@ -4,13 +4,35 @@ using UnityEngine;
 
 public class changecolor : MonoBehaviour
 {
-    public int pos_x = 5;
-    void Update()
+    private bool tempo = true;    
+    private void FixedUpdate()
     {
-        if(Input.GetKeyDown(KeyCode.Q))
-        GetComponent<Renderer>().material.color = Color.blue;
-        
-        if(Input.GetKeyDown(KeyCode.W))
-        GetComponent<Renderer>().material.color = Color.green;
+        if (tempo)
+        {
+            StartCoroutine(ExampleCoroutine());
+            tempo = false;
+        }
+    }
+    public void MudaCor(bool cor = true)
+    {
+        if (cor == true)
+            GetComponent<Renderer>().material.color = Random.ColorHSV(0f, 1f, 1f, 1f, 0.5f, 1f);
+        else
+            GetComponent<Renderer>().material.color = Random.ColorHSV(0f, 1f, 1f, 1f, 0.5f, 1f);
+    }
+    IEnumerator ExampleCoroutine()
+    {
+        //Print the time of when the function is first called. color red
+        Debug.Log("Started Coroutine at timestamp : " + Time.time);
+        MudaCor(true);
+
+        //yield on a new YieldInstruction that waits for 5 seconds.
+        yield return new WaitForSeconds(0.5f);
+
+        //After we have waited seconds print the time again. color blue
+        Debug.Log("Finished Coroutine at timestamp : " + Time.time);
+        MudaCor(false);
+        yield return new WaitForSeconds(0.5f);
+        tempo = true;
     }
 }
